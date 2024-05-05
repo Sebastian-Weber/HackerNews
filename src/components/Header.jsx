e3f73f90ef76014d9654dacf0ae6ea175c5c1ad4
Sebastian Weber <s-weber85@gmx.de>
2024-05-05 10:22:00 +0200

    header styling added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,18 +24,30 @@ function Header(props) {
 	}
 
 	return (
-		<div className="bg-amber-500 p-1">Hello HackerNews ...
-			<form onSubmit={handleSubmit}>
-				<div className="flex gap-2">
-					<input type="text" ref={inputRef} />
-					<div>
-						<a className="cursor-pointer" onClick={handleNew}>new</a>&nbsp;|&nbsp;
-						<a className="cursor-pointer" onClick={handleAsk}>ask</a>&nbsp;|&nbsp;
-						<a className="cursor-pointer" onClick={handleShow}>show</a>
+		<>
+		{/* ReactNews Header */}	
+		<div className="flex flex-nowrap bg-gray-800" >
+			<div class="items-center" >
+				<img class="my-4 pt-4 pr-2 pl-4 size-full md:size-auto" src="./src/assets/react.svg"></img>
+			</div>
+	
+			<h1 className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">React|</h1>
+			<h1 className="mb-4 my-4 pt-1 text-4xl font-mono font-semibold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">News</h1>
+			<br/>
+			<div>
+				<form onSubmit={handleSubmit}>
+					<div className="flex gap-2">
+						<input type="text" ref={inputRef} />
+						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleNew}>new |</a>
+						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleAsk}>ask |</a>
+						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleShow}>show</a>
 					</div>
-				</div>
-			</form>
+				</form>
+			</div>
 		</div>
+
+
+		</>	
 	)
 }
 
