a8a44a1b8f90322f1ccc7de2a225992ddef85e19
Sebastian Weber <s-weber85@gmx.de>
2024-05-05 11:40:03 +0200

    more styling added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,26 +25,47 @@ function Header(props) {
 
 	return (
 		<>
-		{/* ReactNews Header */}	
-		<div className="flex flex-nowrap bg-gray-800" >
-			<div class="items-center" >
-				<img class="my-4 pt-4 pr-2 pl-4 size-full md:size-auto" src="./src/assets/react.svg"></img>
+		{/* Header */}	
+		<div className="p-2 bg-gray-800" >
+
+			<div className="flex flex-nowrap row">
+				{/* Icon */}	
+				<div class="items-center" >
+					<img class="my-4 pt-4 pr-2 pl-4 size-full md:size-auto" src="./src/assets/react.svg"></img>
+				</div>
+				{/* Heading */}
+				<h1 className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">Hacker|</h1>
+				<h1 className="mb-4 my-4 pt-1 text-4xl font-mono font-semibold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">News</h1>
 			</div>
-	
-			<h1 className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">React|</h1>
-			<h1 className="mb-4 my-4 pt-1 text-4xl font-mono font-semibold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">News</h1>
-			<br/>
-			<div>
+		{/* Header nav bar */}	
+		<div className="my-2 mx-5 flex flex-nowrap justify-between row">
+			<div className="flex flex-nowrap row justify-start">
+				<div className="flex pl-9">
+					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
+						<a className="px-2 cursor-pointer" onClick={handleNew}>| most recent |</a>
+					</h5>
+					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
+						<a className="px-2 cursor-pointer" onClick={handleAsk}>| ask |</a>
+						</h5>
+					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
+						<a className="px-2 cursor-pointer" onClick={handleShow}>| archive |</a>
+					</h5>
+				</div>
+			</div>
+			<div className="flex flex-nowrap row justify-end">
 				<form onSubmit={handleSubmit}>
-					<div className="flex gap-2">
-						<input type="text" ref={inputRef} />
-						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleNew}>new |</a>
-						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleAsk}>ask |</a>
-						<a className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white cursor-pointer" onClick={handleShow}>show</a>
+					<div className="flex pr-2">
+						<input type="text" placeholder="Search ..." ref={inputRef} />
 					</div>
 				</form>
 			</div>
+		
 		</div>
+		</div>
+
+		<br/>
+		
+
 
 
 		</>	
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -66,7 +66,7 @@ function NewsList({searchQuery}) {
 							<br/>
 							<p className="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis a minus quae aspernatur, commodi sunt suscipit iure laboriosam ab minima saepe aut necessitatibus autem ullam illum recusandae ut ducimus eum.</p>
 							<br/>
-							{/* Generate button bar */}
+							{/* ButtonBar component: renders a row of buttons */}
 							<div className="flex flex-nowrap justify-items-start">
 
 								{/* Read more button */}
@@ -78,26 +78,26 @@ function NewsList({searchQuery}) {
 									onClick={() => getCommentCount(entry.objectID)}>
 									Read more
 									<div className="flex flex-nowrap justify-between">
-									<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-									</svg>
+										<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+											<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+										</svg>
 									</div>
 									</a>
 								</div> 
 
-								{/* Comments button */}
+								{/* comments button */}
 								<div className="mr-2">
 									<a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
 									Comments {entry.num_comments}
 									<div className="flex flex-nowrap justify-between">
 										<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
-										<path stroke="currentColor" d="M0,1H16V12H5.711L2,15.711V12H0ZM15,11V2H1v9H3v2.289L5.289,11Z" />
+											<path stroke="currentColor" d="M0,1H16V12H5.711L2,15.711V12H0ZM15,11V2H1v9H3v2.289L5.289,11Z" />
 										</svg>
 									</div>
 									</a>
 								</div>
 
-								{/* More from this author button */}
+								{/* author button */}
 								<div className="mr-2">
 								<a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
 								{entry.author}
@@ -107,6 +107,26 @@ function NewsList({searchQuery}) {
 									</a>
 								</div>
 
+								{/* date button */}
+								{/* <div className="mr-2">
+								<a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+								{entry.stringDate}
+									<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+										<path stroke="currentColor" d="M10.391,9.4a6.693,6.693,0,0,1,1.918,1.027,6.785,6.785,0,0,1,1.453,1.527,6.964,6.964,0,0,1,.918,1.9A7.2,7.2,0,0,1,15,16H14a6.127,6.127,0,0,0-.457-2.379,5.752,5.752,0,0,0-3.164-3.164,6.349,6.349,0,0,0-3.988-.246,6.029,6.029,0,0,0-1.441.6,5.86,5.86,0,0,0-2.141,2.141,6.029,6.029,0,0,0-.6,1.441A6.089,6.089,0,0,0,2,16H1a7.009,7.009,0,0,1,1.258-4.039,6.865,6.865,0,0,1,1.457-1.516,7.09,7.09,0,0,1,1.91-1.039,4.913,4.913,0,0,1-1.094-.8,4.964,4.964,0,0,1-.824-1.043A5.121,5.121,0,0,1,3.184,6.34a5.027,5.027,0,0,1,.207-3.289A5,5,0,0,1,6.051.391a5.059,5.059,0,0,1,3.9,0,5,5,0,0,1,2.66,2.66,5.015,5.015,0,0,1,.207,3.285,5.066,5.066,0,0,1-.523,1.219,5.172,5.172,0,0,1-.82,1.043A4.837,4.837,0,0,1,10.391,9.4ZM4,5a3.851,3.851,0,0,0,.316,1.555A4.052,4.052,0,0,0,6.445,8.684a3.978,3.978,0,0,0,3.109,0,4.052,4.052,0,0,0,2.129-2.129A3.851,3.851,0,0,0,12,5a3.851,3.851,0,0,0-.316-1.555A4.052,4.052,0,0,0,9.555,1.316a3.978,3.978,0,0,0-3.109,0A4.052,4.052,0,0,0,4.316,3.445,3.851,3.851,0,0,0,4,5Z"/>
+									</svg>
+									</a>
+								</div> */}
+
+								{/* ID button */}
+								{/* <div className="mr-2">
+								<a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+								{entry.stringDate}
+									<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+										<path stroke="currentColor" d="M10.391,9.4a6.693,6.693,0,0,1,1.918,1.027,6.785,6.785,0,0,1,1.453,1.527,6.964,6.964,0,0,1,.918,1.9A7.2,7.2,0,0,1,15,16H14a6.127,6.127,0,0,0-.457-2.379,5.752,5.752,0,0,0-3.164-3.164,6.349,6.349,0,0,0-3.988-.246,6.029,6.029,0,0,0-1.441.6,5.86,5.86,0,0,0-2.141,2.141,6.029,6.029,0,0,0-.6,1.441A6.089,6.089,0,0,0,2,16H1a7.009,7.009,0,0,1,1.258-4.039,6.865,6.865,0,0,1,1.457-1.516,7.09,7.09,0,0,1,1.91-1.039,4.913,4.913,0,0,1-1.094-.8,4.964,4.964,0,0,1-.824-1.043A5.121,5.121,0,0,1,3.184,6.34a5.027,5.027,0,0,1,.207-3.289A5,5,0,0,1,6.051.391a5.059,5.059,0,0,1,3.9,0,5,5,0,0,1,2.66,2.66,5.015,5.015,0,0,1,.207,3.285,5.066,5.066,0,0,1-.523,1.219,5.172,5.172,0,0,1-.82,1.043A4.837,4.837,0,0,1,10.391,9.4ZM4,5a3.851,3.851,0,0,0,.316,1.555A4.052,4.052,0,0,0,6.445,8.684a3.978,3.978,0,0,0,3.109,0,4.052,4.052,0,0,0,2.129-2.129A3.851,3.851,0,0,0,12,5a3.851,3.851,0,0,0-.316-1.555A4.052,4.052,0,0,0,9.555,1.316a3.978,3.978,0,0,0-3.109,0A4.052,4.052,0,0,0,4.316,3.445,3.851,3.851,0,0,0,4,5Z"/>
+									</svg>
+									</a>
+								</div> */}
+
 								{/* Tags button */}
 								{/* <a className="inline-flex items-center mx-4 px-3 py-2 text-sm font-medium text-center text-white bg-sky-500 rounded-lg hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
 								Tags: {entry._tags}
@@ -115,7 +135,7 @@ function NewsList({searchQuery}) {
 								</div>
 								<br/>
 
-								{/* Card footer*/}
+								{/* CardFooter component: renders the card footer with dates*/}
 								<div className="flex flex-nowrap justify-between">
 
 								{/* Created at */}
