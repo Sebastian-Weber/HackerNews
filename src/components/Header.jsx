5a2d457af5a3c148a18c7be879c8f2cc31c504f6
Sebastian Weber <s-weber85@gmx.de>
2024-05-05 13:14:20 +0200

    more styling added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,31 +28,31 @@ function Header(props) {
 		{/* Header */}	
 		<div className="p-2 bg-gray-800" >
 
-			<div className="flex flex-nowrap row">
-				{/* Icon */}	
+			<div className="flex row">
+				{/* Logo */}	
 				<div class="items-center" >
 					<img class="my-4 pt-4 pr-2 pl-4 size-full md:size-auto" src="./src/assets/react.svg"></img>
 				</div>
 				{/* Heading */}
-				<h1 className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">Hacker|</h1>
-				<h1 className="mb-4 my-4 pt-1 text-4xl font-mono font-semibold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">News</h1>
+					<h1 className="mb-4 my-4 text-4xl font-sans font-regular leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">Hacker|</h1>
+					<h1 className="mb-4 my-4 pt-1 text-4xl font-mono font-semibold leading-none tracking-tight text-gray-200 md:text-5xl lg:text-6xl dark:text-white">News</h1>
 			</div>
 		{/* Header nav bar */}	
-		<div className="my-2 mx-2 flex flex-nowrap justify-between row">
-			<div className="flex flex-nowrap row justify-start">
-				<div className="flex px-12">
+		<div className="my-2 mx-2 flex justify-between row">
+			<div className="flex row justify-start">
+				<div className="flex px-2">
 					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
-						<a className="px-2 cursor-pointer" onClick={handleNew}>| most recent |</a>
+						<a className="px-2 cursor-pointer" onClick={handleNew}>most recent</a>
 					</h5>
 					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
-						<a className="px-2 cursor-pointer" onClick={handleAsk}>| ask |</a>
+						<a className="px-2 cursor-pointer" onClick={handleAsk}>ask</a>
 						</h5>
 					<h5 className="text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
-						<a className="px-2 cursor-pointer" onClick={handleShow}>| archive |</a>
+						<a className="px-2 cursor-pointer" onClick={handleShow}>archive</a>
 					</h5>
 				</div>
 			</div>
-			<div className="flex flex-nowrap row justify-end">
+			<div className="flex row justify-end">
 				<form onSubmit={handleSubmit}>
 					<div className="flex px-4">
 						<input type="text" placeholder="Search ..." ref={inputRef} />
