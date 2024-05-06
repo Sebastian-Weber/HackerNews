6422d75a00a8ecb85176efde0e4d6e459984bceb
Sebastian Weber <s-weber85@gmx.de>
2024-05-06 10:15:41 +0200

    Pagination colors changed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -24,7 +24,7 @@ function Pagination() {
 						<a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
 						</li>
 						<li>
-						<a href="#" aria-current="page" class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+						<a href="#" aria-current="page" class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-sky-400 border border-bsky-300 bg-blue-50 hover:bg-sky-100 hover:text-sky-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
 						</li>
 						<li>
 						<a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
