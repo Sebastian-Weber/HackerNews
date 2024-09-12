fc2cd1fc302836944901597e13bd54659b285957
Sebastian Weber <s-weber85@gmx.de>
2024-09-12 10:57:12 +0200

    Refactor layout and clean up code in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ function Header(props) {
 		{/* Header */}	
 		<div className="p-2 bg-gray-800 sticky top-0 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 
-			<div className="flex flex-row mx-4 my-4 items-center">
+			<div className="flex flex-row mx-4 items-center">
 				{/* Logo */}	
 					<div className="mb-4 my-4 size-12 bg-gray-100">
 						{/* <img class="my-4 pt-4 pr-2 pl-4 size-full md:size-auto" src="./src/assets/react.svg"></img> */}
@@ -45,9 +45,8 @@ function Header(props) {
 					<h2 className="pl-1 mb-4 my-4 font-sans font-semibold leading-none tracking-tight cursor-default select-none text-gray-200 text-4xl md:text-5xl lg:text-6xl dark:text-white">News</h2>
 			</div>
 		{/* Header nav bar */}	
-		<div className="flex flex-wrap my-2 mx-2 justify-between ">
-			<div className="flex flex-row justify-start">
-				<div className="flex flex-wrap px-2">
+		<div className="flex flex-wrap mx-2 py-4 items-start justify-between">
+				<div className="flex flex-wrap items-start">
 					<h5 className="flex flex-row text-1xl font-normal tracking-tight text-gray-200 dark:text-white">
 						<a className="px-2 cursor-pointer" onClick={handleNew}>| most recent |</a>
 					</h5>
@@ -58,15 +57,14 @@ function Header(props) {
 						<a className="px-2 cursor-pointer" onClick={handleShow}>| archive |</a>
 					</h5>
 				</div>
-			</div>
-			<div className="flex flex-row justify-end">
-				<form onSubmit={handleSubmit}>
-					<div className="px-4 mt-0">
-						<input class="w-28 md:min-w-36 lg:min-w-44 px-1 placeholder-gray-900 focus:ring-" type="text" placeholder="&nbsp;Search ..." ref={inputRef}/>
-					</div>
-				</form>
-			</div>
-		
+
+		</div>
+		<div className="flex flex-row items-end justify-end">
+			<form onSubmit={handleSubmit}>
+				<div className="m-2">
+					<input class="w-28 md:min-w-36 lg:min-w-44 px-1 placeholder-gray-900 focus:ring-1" type="text" placeholder="&nbsp;Search ..." ref={inputRef}/>
+				</div>
+			</form>
 		</div>
 		</div>
 
